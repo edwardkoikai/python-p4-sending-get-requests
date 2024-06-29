5224ae7330a9b54b23e3f950bb3c9ea0df70381c
edwardkoikai <edwardkoikai@gmail.com>
2024-06-29 21:44:23 +0300

    install requests

--- lib/get.py
+++ lib/get.py
@@ -5,6 +5,10 @@ url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locatio
 
 response = requests.get(url)
 
+# json.loads(response.content)
+
 json_content = json.loads(response.text)
 
 print(json.dumps(json_content, indent=4))
+
+# print(response.text)
